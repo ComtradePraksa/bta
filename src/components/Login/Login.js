531cb9a9e077258208ba5e5e58ca176147946df1
Danilo3110 <lukic3@sbb.rs>
2019-06-13 00:06:14 +0200

    small changes

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import styles from './Login.css';
+import classes from './Login.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Login extends Component {
@@ -10,21 +10,22 @@ class Login extends Component {
     //feedback for user(message)
     feedback: ''
   }
-  handleInput = (e)=> {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-  login = (e) => {
-    let user = {
+
+  inputHandler = e => {
+    this.setState ({ [e.target.name]: e.target.value });
+  };
+
+  login = e => {
+    const user = {
       username: this.state.username,
       password: this.state.password
-    }
+    };
     e.preventDefault();
+
     //no empty fields allowed!!!
     if(!user.username || !user.password){
       //display feedback
-      this.feedback = 'Username and password are required!'
+      this.setState({ feedback: 'Username and password are required!' });
     }
     else{
       //check if there is a user with these credentials
@@ -32,60 +33,49 @@ class Login extends Component {
         method: 'post',
         url: 'http://localhost:3001/users',
         data: user,
-        config: {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        }
+        config: { headers: { 'Content-Type': 'application/json' } }
       })
-      .then(res=> {
+      .then(res => {
         //there is a user in db
-        if (res.data.data.length){
+        if (res.data.data.length) {
           //redirect to a home page
-          console.log(res.data.data[0].username);
-          console.log(res.data.data[0].password);
-          console.log(res.data.data[0].id);
-          console.log(res.data.data[0].is_admin);
-          this.setState({
-            feedback: `Logged in as ${res.data.data[0].username}`
-          })
+          this.setState({ feedback: `Logged in as ${res.data.data[0].username}` });
         }
-        else{
-          this.setState({
-            feedback: 'no user with that credentials'
-          })
+        else {
+          this.setState({ feedback: 'no user with that credentials' });
         }
       })
-      .catch(err=>{console.log(err)})
+      .catch(err=>{console.log(err)});
     }
   }
+
   render() {
     return (
-      <div className= {styles.loginWrapper} >
-        <form onSubmit = {this.login}>
-          <div className={styles.formHeader}>
+      <div className={classes.loginWrapper} >
+        <form onSubmit={this.login}>
+          <div className={classes.formHeader}>
             Login to BTA
           </div>
-          <div className= {styles.formGroup}>
-            <div className={styles.inputWrapper}>
-              <div className={styles.iconWrapper}>
-              <FontAwesomeIcon icon="user" />
+          <div className={classes.formGroup}>
+            <div className={classes.inputWrapper}>
+              <div className={classes.iconWrapper}>
+                <FontAwesomeIcon icon="user" />
               </div>
-              <input onChange = {this.handleInput} type="text" name="username" placeholder="username"/>
+              <input onChange={this.inputHandler} type="text" name="username" placeholder="username"/>
             </div>
           </div>
-          <div className={styles.formGroup}>
-            <div className={styles.inputWrapper}>
-              <div className={styles.iconWrapper}>
-              <FontAwesomeIcon icon="key" />
+          <div className={classes.formGroup}>
+            <div className={classes.inputWrapper}>
+              <div className={classes.iconWrapper}>
+                <FontAwesomeIcon icon="key" />
               </div>
-              <input onChange = {this.handleInput} type="password" name="password" placeholder="password"/>
+              <input onChange={this.inputHandler} type="password" name="password" placeholder="password"/>
             </div>
           </div>
-          <div className = "feedback">
+          <div className="feedback">
             {this.state.feedback}
           </div>
-          <button className={styles.btn}>Login</button>
+          <button className={classes.btn}>Login</button>
         </form>
       </div>
     );
