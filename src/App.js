e329c39f864cd8ff9097f9950c965343be4738c0
darkomingovski <darko.mingovski@gmail.com>
2019-06-13 12:51:47 +0200

    pulling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import classes from './App.css';
 import Login from './components/Login/Login';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {  faKey, faUser } from '@fortawesome/free-solid-svg-icons';
+import {getHotel} from './apis/hotelScrapersApi.js';
+import Main from './components/Main/Main';
 
 library.add( faKey, faUser);
 
@@ -12,14 +13,15 @@ class App extends Component {
     accomodations: []
   }
 
+  
+
   componentDidMount() {
-    const url = 'https://www.booking.com/hotel/de/hotel-dortmund.html';
-    axios.get(`https://scrappet.herokuapp.com/api/scrape?url=${url}`)
-      .then(res => {
-        const accomodations = res.data.page.meta_tags;
-        this.setState({ accomodations });
-      })
-  };
+     (async () => {
+      const data = await getHotel('https://www.booking.com/hotel/de/hotel-dortmund.html');
+      const accomodations = data.page.meta_tags;
+       this.setState({ accomodations });
+   })();
+  }
          
 
   render() {
@@ -30,6 +32,7 @@ class App extends Component {
         <div>{this.state.accomodations['og:description']}</div>
         <div><img src={this.state.accomodations['og:image']} alt="Hotel"></img></div>
         <div>{this.state.accomodations['og:url']}</div>
+        <Main />
       </div>
     );
   }
