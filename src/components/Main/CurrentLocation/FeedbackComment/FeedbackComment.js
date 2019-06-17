f7ce2e20e83c6f98adedc827b79f61849bd69eaa
martinatrajkovic <martina.tr@outlook.com>
2019-06-17 20:48:47 +0200

    comments added

---
+++ src/components/Main/CurrentLocation/FeedbackComment/FeedbackComment.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import classes from "./FeedbackComment.css"
+import {formatDate } from "../FeedbackFunction/FeedbackFunction"
+
+
+
+class FeedbackComment extends Component{
+    render(){
+        return(
+            <div className={classes.commentContainer}>
+                <div className={classes.userName}>
+                    <p>{this.props.comments.name}</p>
+                </div>
+                <div className={classes.userPicture}>
+                    <img src={require(`../../../../${this.props.comments.photo}`)} alt="profilePicture"/>
+                </div>
+                <div className={classes.commentDate}>{formatDate(this.props.comments.comment_date)}</div>
+                <div className={classes.commentText}>{this.props.comments.comments}</div>
+
+            </div>
+        )
+    }
+}
+export default FeedbackComment
