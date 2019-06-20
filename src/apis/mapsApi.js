0c1fe40877edfef7a723083d0a7266fe994d0f66
darkomingovski <darko.mingovski@gmail.com>
2019-06-21 01:04:59 +0200

    Lekic

--- src/apis/mapsApi.js
+++ src/apis/mapsApi.js
@@ -9,16 +9,14 @@ const map = (latitude, longitude, location) => {
         center: { lat: latitude, lng: longitude },
         zoom: 12,
     });
+    /*eslint-disable */
     const ui = window.H.ui.UI.createDefault(map, layer);
     const marker = new window.H.map.Marker({ lat: latitude, lng: longitude });
     map.addObject(marker); 
    if(location !== undefined){
        location.map((e,index)=> map.addObject(new window.H.map.Marker({ lat: location[index].position[0], lng: location[index].position[1] },{icon:new window.H.map.Icon(location[index].icon)})))
    }
-    /*eslint-disable */
     
-   
-   
     const mapEvents = new window.H.mapevents.MapEvents(map);  
     const behavior = new window.H.mapevents.Behavior(mapEvents);
     /*eslint-enable */
