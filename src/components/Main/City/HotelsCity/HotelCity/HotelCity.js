15728277fb5d8d7ce6bd1ae7e7ea0380fe71230f
GospodinKrompir <alek988@gmail.com>
2019-06-21 02:37:13 +0200

    prepravljena HotelCity komponenta

--- src/components/Main/City/HotelsCity/HotelCity/HotelCity.js
+++ src/components/Main/City/HotelsCity/HotelCity/HotelCity.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './HotelCity.css';
 
-class HotelsCity extends Component {
-   
-    render() {
 
-        return (
-            <div className={classes.Nearby}>
-                        <img src={`${this.props.hotel.hotel_img}`} alt={this.props.hotel.image} />
-                        <h3>{this.props.hotel.name}</h3>
-                        <p>{this.props.hotel.hotel_descr}</p>
-                        <p className={classes.address}></p>
-                    </div>
-        )
-    }
+const HotelCity = props => {
+    return (
+        <div className={classes.Nearby}>
+            <img src={`${props.hotel.hotel_img}`} alt={props.hotel.image} />
+            <h3>{props.hotel.name}</h3>
+            <p>{props.hotel.hotel_descr}</p>
+            <p className={classes.address}></p>
+        </div>
+    )
 }
 
-export default HotelsCity;
+
+export default HotelCity;
