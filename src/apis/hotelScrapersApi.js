7eacab54e9bd357186eeffd8d3c5d33d79761a7c
Danilo3110 <lukic3@sbb.rs>
2019-06-21 02:52:27 +0200

    code formatting

--- src/apis/hotelScrapersApi.js
+++ src/apis/hotelScrapersApi.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const url = `https://davidesantangelo-scrappet-v1.p.rapidapi.com/api/scrape?url=`;
-const headers ={
+const headers = {
     "X-RapidAPI-Host": "davidesantangelo-scrappet-v1.p.rapidapi.com",
     "X-RapidAPI-Key" : "98533b8f86mshe857bd09d846a6cp1cf65ejsn823f3051d35c"
+};
 
-}
 const getHotel = async (hotelUrl) => {
     try {
-        const response = await axios.get(`${url}${hotelUrl}`,{headers});
+        const response = await axios.get(`${url}${hotelUrl}`, {headers});
         const hotel = await response.data;
         return hotel;
     } catch (error) {
@@ -16,5 +16,4 @@ const getHotel = async (hotelUrl) => {
     }
 };
 
-export {getHotel}
-
+export {getHotel};
