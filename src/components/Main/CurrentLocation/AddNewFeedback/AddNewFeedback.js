451b82a45618289310bdd2b18a06694619a657fe
martinatrajkovic <martina.tr@outlook.com>
2019-06-21 12:11:31 +0200

    add new feedback popup

---
+++ src/components/Main/CurrentLocation/AddNewFeedback/AddNewFeedback.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import classes from "./AddNewFeedback.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+class AddNewFeedback extends Component {
+
+    render() {
+        return (
+            <div className={classes.newFeedbackWindow}>
+                <div className={classes.newFeedbackPopup}>
+                    <div className={classes.newFeedbackHeader}>
+                        <FontAwesomeIcon icon="times" onClick={this.props.toggle} />
+                    </div>
+                    <div>
+
+                    </div>
+           
+                   
+                </div>
+
+            </div>
+        )
+    }
+}
+export default AddNewFeedback
