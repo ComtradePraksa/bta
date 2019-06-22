ab457c70f9832fcb492416cdaa4067ff293d6f51
Danilo3110 <lukic3@sbb.rs>
2019-06-22 22:42:51 +0200

    small changes - formatting

--- src/components/Main/AdminPanel/AdminPanel.js
+++ src/components/Main/AdminPanel/AdminPanel.js
@@ -10,11 +10,13 @@ import classes from './AdminPanel.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class AdminPanel extends Component {
+    
     logout = () =>{
         //logout
         this.props.loginStatus(false, {});
         localStorage.removeItem('jwtoken');
-    }
+    };
+
     render() {
         if (!this.props.loggedUser.is_admin) {
             return <Redirect to="/home"/>;
