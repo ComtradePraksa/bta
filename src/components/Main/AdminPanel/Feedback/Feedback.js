d3247979f614903041c4f2ca80b4de921c52a734
Danilo3110 <lukic3@sbb.rs>
2019-06-26 03:08:40 +0200

    small changes in adminPanel css

--- src/components/Main/AdminPanel/Feedback/Feedback.js
+++ src/components/Main/AdminPanel/Feedback/Feedback.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import {getFromDatabase} from '../../../../apis/btaApi';
+import FeedbackTicket from '../../FeedbackContainer/FeedbackTicket/FeedbackTicket';
 import classes from './Feedback.css';
 
 class Feedback extends Component {
     state = {
-        location_feedbacks: '',
-        acc_feedbacks: '',
-        transportation_feedbacks: '',
+        // location_feedbacks: '',
+        // acc_feedbacks: '',
+        // transportation_feedbacks: '',
+        feedbacks: '',
         regEx_message: ''
     };
 
@@ -21,9 +23,14 @@ class Feedback extends Component {
     render() {
         return (
             <div className={classes.Feedback}>
-                <div onClick={() => this.getDatabase('/location_feedbacks', 'location_feedbacks')}>Location Feedbacks</div>
-                <div onClick={() => this.getDatabase('/acc_feedbacks', 'acc_feedbacks')}>Accommodation Feedbacks</div>
-                <div onClick={() => this.getDatabase('/transportation_feedbacks', 'transportation_feedbacks')}>Transportation Feedbacks</div>
+                <div onClick={() => this.getDatabase('/location_feedbacks', 'feedbacks')}>Location Feedbacks</div>
+                <div onClick={() => this.getDatabase('/acc_feedbacks', 'feedbacks')}>Accommodation Feedbacks</div>
+                <div onClick={() => this.getDatabase('/transportation_feedbacks', 'feedbacks')}>Transportation Feedbacks</div>
+                <p>
+                {   (this.state.feedbacks !== '') ? 
+                    this.state.feedbacks.map((fb,index) => (<FeedbackTicket loggedUser={this.props.loggedUser} key={index} fb={fb}/>)) : null 
+                }
+                </p>
             </div>
         )
     }
