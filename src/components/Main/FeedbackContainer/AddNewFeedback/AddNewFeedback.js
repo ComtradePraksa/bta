fa6270061e138d92dcd3b13b676f9c34c65909da
9999starlight <sestakirena@gmail.com>
2019-06-26 07:28:23 +0200

    updated feedback css

--- src/components/Main/FeedbackContainer/AddNewFeedback/AddNewFeedback.js
+++ src/components/Main/FeedbackContainer/AddNewFeedback/AddNewFeedback.js
@@ -30,52 +30,52 @@ class AddNewFeedback extends Component {
 
     render() {
         return (
-            <div className={classes.newFeedbackWindow}>
+            <div className={[classes.newFeedbackWindow, classes.flex, classes.center, classes.fullWidth].join(' ')}>
                 <div className={classes.newFeedbackPopup}>
-                    <div className={classes.newFeedbackHeader}>
+                    <div className={[classes.newFeedbackHeader, classes.flex].join(' ')}>
                         <FontAwesomeIcon icon="times" onClick={this.props.toggle} />
-                        <button className={classes.saveFeedback}>SAVE</button>
+                        <button className={[classes.saveFeedback, classes.pointer].join(' ')}>SAVE</button>
                     </div>
                     <div className={classes.newFeedbackMain}>
-                        <div className={classes.inputTitle}>
-                            <input className={classes.title} type="text" placeholder="Enter Title" />
+                        <div className={[classes.inputTitle, classes.flex].join(' ')}>
+                            <input className={[classes.title, classes.flex, classes.fullWidth].join(' ')} type="text" placeholder="Enter Title" />
                         </div>
                         <div className={classes.chooseType}>
                         
                             <div className={classes.dropdownWrapper}>
-                                <div className={classes.dropdown} >
+                                <div className={[classes.dropdown, classes.fullWidth].join(' ')} >
                                     <div className={classes.overlay}></div>
                                     <div className={classes.dropdownTileWrapper}>
-                                        <input placeholder="Category" className={classes.categoryInput}  readOnly onClick={this.toggle}/>
+                                        <input placeholder="Category" className={[classes.categoryInput, classes.pointer].join(' ')}  readOnly onClick={this.toggle}/>
                                         <FontAwesomeIcon icon="chevron-down" style={{ color: '#gray' }} />
                                     </div>
                                     {this.state.dropdownVisible && <ul className={classes.dropdownItemList}>
-                                        <li className={classes.dropdownItem} onClick={this.getCategory}>Food</li>                                        
-                                        <li className={classes.dropdownItem} onClick={this.getCategory}>Transportation</li>
-                                        <li className={classes.dropdownItem} onClick={this.getCategory}>Culture</li>
-                                        <li className={classes.dropdownItem} onClick={this.getCategory}>Safety</li>
-                                        <li className={classes.dropdownItem} onClick={this.getCategory}>Sightseeing</li>
-                                        <li className={classes.dropdownItem} onClick={this.getCategory}>Other</li>
+                                        <li className={[classes.dropdownItem, classes.flex, classes.center, classes.pointer].join(' ')} onClick={this.getCategory}>Food</li>                                        
+                                        <li className={[classes.dropdownItem, classes.flex, classes.center, classes.pointer].join(' ')} onClick={this.getCategory}>Transportation</li>
+                                        <li className={[classes.dropdownItem, classes.flex, classes.center, classes.pointer].join(' ')} onClick={this.getCategory}>Culture</li>
+                                        <li className={[classes.dropdownItem, classes.flex, classes.center, classes.pointer].join(' ')} onClick={this.getCategory}>Safety</li>
+                                        <li className={[classes.dropdownItem, classes.flex, classes.center, classes.pointer].join(' ')} onClick={this.getCategory}>Sightseeing</li>
+                                        <li className={[classes.dropdownItem, classes.flex, classes.center, classes.pointer].join(' ')} onClick={this.getCategory}>Other</li>
                                     </ul> }
                                 </div>
                             </div>
                         </div>
-                        <div className={classes.feedbackRate}>
-                            <div className={classes.title}>
+                        <div className={[classes.feedbackRate, classes.fullWidth].join(' ')}>
+                            <div className={[classes.title, classes.flex].join(' ')}>
                                 Your rate
                         </div>
-                            <div className={classes.rateContainer}>
+                            <div className={[classes.rateContainer, classes.flex, classes.center].join(' ')}>
                                 <FontAwesomeIcon id="iconmove" style={{ height: "40px", width: "40px", left: `${this.state.rate * 10}%` }} icon="map-marker" />
                                 <p style={{ left: `${this.state.rate * 10}%` }} id="text">{this.state.rate}</p>
-                                <input type="range" min="0" max="10" step="0.1" onChange={this.getRate} value={this.state.rate} />
+                                <input type="range" min="0" max="10" step="0.1" onChange={this.getRate} value={this.state.rate} className={[classes.fullWidth, classes.pointer].join(' ')}/>
                             </div>
                         </div>
                         <div className={classes.feedbackField}>
-                        <div className={classes.title}>
+                        <div className={[classes.title, classes.flex].join(' ')}>
                         Your feedback
                         </div>
-                        <div className={classes.textarea}>
-                        <textarea onChange={this.getFeedbackValue} placeholder="Enter your feedback"/>
+                        <div className={[classes.textarea, classes.flex, classes.center].join(' ')}>
+                                <textarea onChange={this.getFeedbackValue} placeholder="Enter your feedback" className={classes.fullWidth}/>
                         </div>
                         </div>
                     </div>
